d03032e41b0c7b43ff9a412a6692d0b87efdffc5
Edward <edpraveen1@gmail.com>
2023-05-24 16:25:49 +0530

    .

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -8,36 +8,56 @@
 fn main() {
     // primitive types - int, float
     // ownership stays- copying the values automatically
-    let x:f64 = 32.6;
-    let y:f64 = x;
+    let x: f64 = 32.6;
+    let y: f64 = x;
 
-    println!("x: {}, y:{}",x,y); 
+    println!("x: {}, y:{}", x, y);
 
     // Non -primitive types - Vector, Strings
     // Ownership issue occurs - clone or reference to the actual owner
     // Either referencing to the actual owner by `&` or by cloning values
 
-
-
     let s1 = String::from("hello world!");
     // let s2 = s1;
     // println!("S1: {}, S2: {}",s1,s2); // issue occurs
-    let s2 = &s1; 
-    println!("S1: {}, S2: {}",s1,s2);
+    let s2 = &s1;
+    println!("S1: {}, S2: {}", s1, s2);
 
     // Same for vectors
-    let vec_1 = vec![5,6,7,8,9];
+    let vec_1 = vec![5, 6, 7, 8, 9];
     let vec_2 = vec_1.clone(); // returns the copy of value
-    println!("Vec1: {:?}, Vec2: {:?}",vec_1,vec_2);
+    println!("Vec1: {:?}, Vec2: {:?}", vec_1, vec_2);
 
     // When owner goes out of scope,the value will be dropped
     {
         // scope
         let my_name = String::from("Edward Praveen");
-        println!("The name is {}",my_name); // successful printing
+        println!("The name is {}", my_name); // successful printing
     }
 
     // throws error, out of scope value dropped
-    // println!("The name is {}",my_name); 
+    // println!("The name is {}",my_name);
 
+    // Another Scenario
+    let stack_num = 32;
+    let mut heap_vec = vec![4, 5, 6];
+
+    stack_fn(stack_num); // won't change immutable stack_num
+    println!("The value inside the main fn of stack_num: {}", stack_num);
+
+    heap_fn(&heap_vec);
+    // put `&` as reference to heap_vec and in heap_fn to avoid showing below errors
+    // this moves ownership from heap_vec to var
+    // borrow of moved value: `heap_vec`value borrowed here after move
+    // use `&mut` if you want to update the value
+    println!("The value inside the main fn of heap_vec: {:?}", heap_vec);
+}
+
+fn stack_fn(mut var: i32) {
+    var = 56;
+    println!("Var: {}", var);
+}
+
+fn heap_fn(var: &Vec<i32>) {
+    println!("Var: {:?}", var);
 }
