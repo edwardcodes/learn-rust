ab1bfaae40e185e87ab3fb04360a9ab059de7793
Edward <edpraveen1@gmail.com>
2023-05-24 18:20:43 +0530

    .

---
+++ ownership_extended/target/debug/deps/ownership_extended-0d67b08b87b7fa68.d
@@ -0,0 +1,5 @@
+/home/edward/misc/learn-rust/ownership_extended/target/debug/deps/ownership_extended-0d67b08b87b7fa68.rmeta: src/main.rs
+
+/home/edward/misc/learn-rust/ownership_extended/target/debug/deps/ownership_extended-0d67b08b87b7fa68.d: src/main.rs
+
+src/main.rs:
